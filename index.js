5e9e6a6d3f6dea1959e62759bb7422f9d2acbf9e
Emily Gann <aspentree79@gmail.com>
2020-11-11 11:28:14 -0500

    connected backend and frontend

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("in index.js");
+const endPoint = "http://localhost:3000/api/v1/events";
+
+document.addEventListener('DOMContentLoaded', () => {
+   getEvents()
+})
+
+function getEvents() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(events => {
+        // console.log(events);
+        events.data.forEach(event => {
+            const eventInfo = `
+            <div data-id=${event.id}>
+            <h3>${event.attributes.year} - ${event.attributes.title}</h3>
+            <p>${event.attributes.description}</p>
+            </div>
+            <br><br>`;
+
+            document.querySelector('#event-container').innerHTML += eventInfo
+        })
+    })
+}
+
+
