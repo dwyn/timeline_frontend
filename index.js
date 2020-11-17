4dbe6c251caabfdf31e743ab2fe8ebacf2fd8305
Emily Gann <aspentree79@gmail.com>
2020-11-17 16:36:20 -0500

    trying to debug json rendering

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const endPointTimelines = "http://localhost:3000/api/v1/timelines";
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('LOADED');
-    // getTimelines()
-})
+    fetch(endPointTimelines)
+        .then(res => res.json())
+        .then(json => console.log(json));
+    });
+
+            // timeline.data.forEach(timeline => {
+            //     const timelineMarkup = `
+            //         <h4>${timeline.title}</h4>
+            //         <p>${timeline.description}</p>`;
+
+            //     document.querySelector('#displayedTimeline').innerHTML += timelineMarkup;
+            // });
+        // });
+// });
