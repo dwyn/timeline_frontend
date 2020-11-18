17a8f2aa1d64f8beb8946a9f87cf4d742fbaae3a
Emily Gann <aspentree79@gmail.com>
2020-11-18 09:08:38 -0500

    Edited html and js to try to debug timeline data appearing

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const endPointTimelines = "http://localhost:3000/api/v1/timelines";
 
 document.addEventListener('DOMContentLoaded', () => {
+    getTimeline()
+})
+
+function getTimeline() {
     fetch(endPointTimelines)
         .then(res => res.json())
-        .then(json => console.log(json));
-    });
+        .then(timeline => {
+            timeline.data.forEach(timeline => {
+            const timelineMarkup = `
+                <div data-id=${timeline.id}>
+                <h3>${timeline.title}</h3>
+                <p>${timeline.description}</p>
+                <br>
+                <h4>${timeline.attributes.event.year} - ${timeline.attributes.event.title}</h4>
+                <p>${timeline.attributes.event.description}</p>
+                <button data-id=${timeline.id}>Edit</button>
 
-            // timeline.data.forEach(timeline => {
-            //     const timelineMarkup = `
-            //         <h4>${timeline.title}</h4>
-            //         <p>${timeline.description}</p>`;
+                </div>
+                <br><br>`;
 
-            //     document.querySelector('#displayedTimeline').innerHTML += timelineMarkup;
-            // });
-        // });
-// });
+            document.querySelector('#displayedTimeline').innerHTML += timelineMarkup;
+        });
+    })
+}
