f8492c64095a4d2768418ce3df59a3b3e5ff1ff3
Emily Gann <aspentree79@gmail.com>
2020-11-19 14:40:59 -0500

    am able to print events to console, just not to page

--- index.js
+++ index.js
@@ -8,59 +8,30 @@ document.addEventListener('DOMContentLoaded', () => {
 function getTimeline() {
     fetch(endPoint)
     .then(resp => resp.json())
-    .then(timeline => {
+    .then(timeline => { 
         timeline.forEach(timeline => {
             const timelineMarkup = `
                 <div data-id=${timeline.id}>
                 <h3>${timeline.title}</h3>
                 <p>${timeline.description}</p>
+                <p>${timeline.events}</>
                 <br>
-                <h4>${timeline.events.year} - ${timeline.events.title}</h4>
-                <p>${timeline.events.description}</p>
                 <button data-id=${timeline.id}>Edit</button>
-
                 </div>
                 <br><br>`;
+            
+               for (var i = 0; i < timeline.events.length; i++) {
+                    console.log(timeline.events[i]) 
+                    }
+                    
+                
 
                 document.querySelector('#displayedTimeline').innerHTML += timelineMarkup
+            })
         })
-    })
-}
+    }
 
+    function createFormHandler(e) {
+        e.preventDefault()
 
-
-
-// const endPoint = "http://localhost:3000/api/v1/timelines";
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     getTimeline()
-// })
-
-// function getTimeline() {
-//     fetch(endPoint)
-//         .then(res => res.json())
-//         .then(timeline => {
-//             timeline.data.forEach(timeline => {
-//                 debugger
-
-//                 const timelineMarkup = `
-//                 <div data-id=${timeline.id}>
-//                 <h3>${timeline.title}</h3>
-//                 <p>${timeline.description}</p>
-//                 <br>
-//                 <h4>${timeline.attributes.event.year} - ${timeline.attributes.event.title}</h4>
-//                 <p>${timeline.attributes.event.description}</p>
-//                 <button data-id=${timeline.id}>Edit</button>
-
-//                 </div>
-//                 <br><br>`;
-
-//                 document.querySelector('#displayedTimeline').innerHTML += timelineMarkup
-//             })
-//         })
-//     }
-
-//     function createFormHandler(e) {
-//         e.preventDefault()
-
-//     }
+    }
