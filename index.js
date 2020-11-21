605dddd3e02b00f2ab12522bd4b991aa9d630162
Emily Gann <aspentree79@gmail.com>
2020-11-21 10:29:30 -0500

    regrouping and starting anew

--- index.js
+++ index.js
@@ -1,50 +1,9 @@
-const endPoint = "http://localhost:3000/api/v1/timelines"
+const URL = 'http://localhost:3000/timelines';
+const timelineContainer = document.querySelector('#displayed-timeline');
+const eventsContainer = document.querySelector('#events-container');
+const submitTimelineButton = document.querySelector('#submit');
 
-document.addEventListener('DOMContentLoaded', () => {
-    // console.log("LOADED");
-    getTimeline();
+const createTimelineButton = document.querySelector('#createButton');
 
-    const createTimelineForm = document.querySelector("#create-timeline-form")
-    createTimelineForm.addEventListener("submit", (e) => createFormHandler(e))
+const timelineFormContainer = document.querySelector('.container');
 
-})
-
-function getTimeline() {
-    fetch(endPoint)
-    .then(resp => resp.json())
-    .then(timeline => { 
-        timeline.map(timeline => {
-            const timelineMarkup = `
-                <div data-id=${timeline.id}>
-                <h3>${timeline.title}</h3>
-                <p>${timeline.description}</p>
-                <ul>
-                    <li>${timeline.events}</li>
-                </ul>
-                <br>
-                <button data-id=${timeline.id}>Edit</button>
-                </div>
-                <br><br>`;
-            
-                for (var i = 0; i < timeline.events.length; i++) {
-                    console.log(timeline.events[i])
-                }
-               
-
-                document.querySelector('#displayedTimeline').innerHTML += timelineMarkup
-            
-                    })
-                })
-            }
-        
-// formHandler function grabs all of the values input through the timeline form 
-    function createFormHandler(e) {
-        e.preventDefault()
-        const titleInput = document.querySelector('#input-title').value
-        const descriptionInput = document.querySelector('#input-description').value
-        postFetch(titleInput, descriptionInput)
-    }
-
-    function postFetch(title, description) {
-        console.log(title, description);
-    }
