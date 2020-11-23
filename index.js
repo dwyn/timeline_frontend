ca96ad7a88434a419daa52c776e1c9d9d0e5f4db
Emily Gann <aspentree79@gmail.com>
2020-11-23 09:36:25 -0500

    starting back to basics

--- index.js
+++ index.js
@@ -2,70 +2,65 @@ let addTimeline = false;
 const startButton = document.querySelector('#startButton')
 const createButton = document.querySelector('#create-button');
 const URL = 'http://localhost:3000/api/v1/timelines';
-const timelineForm = document.querySelector('#create-timeline-form');
+const timelineForm = document.querySelector('.create-timeline-form');
 const displayedTimeline = document.querySelector('#displayed-timeline');
 
 document.addEventListener("DOMContentLoaded", () => {
-    addEventListenerToStart();
-    getTimeline();
+    console.log('LOADED');
+    
     
 })
 
-function getTimeline() {
-    return fetch(URL)
-    .then(res => res.json())
-}
+document.addEventListener("click", showForm)
 
-function postTimeline(timelineData) {
-    fetch(URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-        },
-        body: JSON.stringify({
-           "title": timelineData.title.value,
-           "description": timelineData.description.value 
-        })
-    })
-    .then(res => res.json())
-    .then((obj_timeline) => {
-        let new_timeline = renderTimeline(obj_timeline)
-        displayedTimeline.append(new_timeline)
-    })
-}
-
-function renderTimeline(timeline) {
-    let h2 = document.createElement('h2')
-    h2.innerText = `${timeline.title}`
-
-    let p = document.createElement('p')
-    p.innerText = `${timeline.description}`
-
-    let divCard = document.createElement('div')
-    divCard.setAttribute('class', 'card')
-    divCard.append(h2, p)
-    displayedTimeline.append(divCard)
-}
-
-function addEventListenerToStart() {
-startButton.addEventListener("click", () => {
-    // only want timeline form to appear if user clicks create button; otherwise, should be hidden
+// function getTimeline() {
+//     return fetch(URL)
+//     .then(res => res.json())
+//     .then(json => {})
+// }
+       
+function showForm() {
     addTimeline = !addTimeline
     if (addTimeline) {
         timelineForm.style.display = "block"
         timelineForm.addEventListener('submit', event => {
             event.preventDefault()
-            postTimeline(event.target)
+            
         })
     } else {
         timelineForm.style.display = "none"
     }
-})
 }
 
-getTimeline().then(timelines => {
-    timelines.forEach(timeline => {
-        renderTimeline(timeline)
-    })
-})
+// function postTimeline(timelineData) {
+//     fetch(URL, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             Accept: 'application/json'
+//         },
+//         body: JSON.stringify({
+//            "title": timelineData.title.value,
+//            "description": timelineData.description.value 
+//         })
+//     })
+//     .then(res => res.json())
+//     .then((obj_timeline) => {
+//         let new_timeline = renderTimeline(obj_timeline)
+//         displayedTimeline.append(new_timeline)
+//     })
+// }
+
+// function renderTimeline(timeline) {
+//     let h2 = document.createElement('h2')
+//     h2.innerText = `${timeline.title}`
+
+//     let p = document.createElement('p')
+//     p.innerText = `${timeline.description}`
+
+//     let divCard = document.createElement('div')
+//     divCard.setAttribute('class', 'card')
+//     divCard.append(h2, p)
+//     displayedTimeline.append(divCard)
+// }
+
