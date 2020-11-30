517c9f4a259b0fa10008cf4d26d64b7d00eff90a
Emily Gann <aspentree79@gmail.com>
2020-11-30 15:46:14 -0500

    timelineForm and createFormHandler are working

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let addTimeline = false;
 const startButton = document.querySelector('#startButton')
-const createButton = document.querySelector('#create-button');
 const URL = 'http://localhost:3000/api/v1/timelines';
 const timelineForm = document.querySelector('.create-timeline-form');
 const newEventUI = document.getElementById("events");
-const addButton = document.getElementById("add-button");
+const addButton = document.getElementById("addEvent")
+const createButton = document.querySelector('#createButton');
 const displayedTimeline = document.querySelector('.card-columns');
 
 
@@ -13,12 +13,26 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log('LOADED');
     getTimeline();
 
-    // timelineForm.addEventListener("submit", (e) => 
-    // e.preventDefault()
-    // ) 
+    
+
+    timelineForm.addEventListener("submit", (e) => 
+    createFormHandler(e))
 })
+    // on submit, new event element should append to show events container
 
 
+    // document.getElementById("addButton").addEventListener('submit', ;
+
+    // formHandler function grabs all of the values input through the timeline form 
+function createFormHandler(e) {
+    e.preventDefault()
+    console.log(e);
+    // const titleInput = document.querySelector('#title').value
+    // const descriptionInput = document.querySelector('#description').value
+    // postFetch(titleInput, descriptionInput)
+}
+
+// GET FETCH REQUEST - GET TIMELINE(S) TO DISPLAY
 function getTimeline() {
     fetch(URL)
     .then(res => res.json())
@@ -39,39 +53,28 @@ function getTimeline() {
             document.querySelector('.card-columns').innerHTML += timelineMarkup
         })
     })
-    
     .catch(error => {
         alert('An error occurred while retrieving some essential timeline info. The error was: ' + error.toString())
     })
 
 
+    // POST FETCH REQUEST
+function postFetch(title, description) {
+    console.log(title, description);
+    const bodyData = {title, description}
+    fetch(URL, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(bodyData)
+    })
+    .then(res => res.json())
+    .then(timeline => {
+        console.log(timeline.bodyData);
+    })
+}
 
 
-
-       
-
-// formHandler function grabs all of the values input through the timeline form 
-// function createFormHandler(e) {
-//     e.preventDefault()
-//     const titleInput = document.querySelector('#title').value
-//     const descriptionInput = document.querySelector('#description').value
-//     postFetch(titleInput, descriptionInput)
-// }
-
-// function postFetch(title, description) {
-//     console.log(title, description);
-//     const bodyData = {title, description}
-//     fetch(URL, {
-//         method: "POST",
-//         headers: {"Content-Type": "application/json"},
-//         body: JSON.stringify(bodyData)
-//     })
-//     .then(res => res.json())
-//     .then(timeline => {
-//         console.log(timeline.bodyData);
-//     })
-// }
-
+// START BUTTON AND FORM TOGGLE
 document.getElementById("startButton").addEventListener('click', formToggle);
 
 function formToggle(button) {
