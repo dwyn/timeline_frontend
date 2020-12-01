19089d2d9aca7b97e8054e612c0f90c7a7acd0f4
Emily Gann <aspentree79@gmail.com>
2020-12-01 15:52:48 -0500

    added post fetch code but working through some bugs

--- index.js
+++ index.js
@@ -30,37 +30,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
        // POST FETCH REQUEST
     function postFetch(timelineTitle, timelineDesc, eventYear, eventTitle, eventDesc) {
-        console.log(timelineTitle, timelineDesc, eventYear, eventTitle, eventDesc);
-    }
+        // console.log(timelineTitle, timelineDesc, eventYear, eventTitle, eventDesc);
         // build body object
-//         let bodyData = {title, description, image_url, category_id}
-      
-//         fetch(endPoint, {
-           // POST request
-//           method: "POST",
-//           headers: {"Content-Type": "application/json"},
-//           body: JSON.stringify(bodyData)
-//         })
-//         .then(response => response.json())
-//         .then(syllabus => {
-//           console.log(syllabus);
-//           const syllabusData = syllabus.data
-           // render JSON response
-//           const syllabusMarkup = `
-//           <div data-id=${syllabus.id}>
-//             <img src=${syllabusData.attributes.image_url} height="200" width="250">
-//             <h3>${syllabusData.attributes.title}</h3>
-//             <p>${syllabusData.attributes.category.name}</p>
-//             <button data-id=${syllabusData.id}>edit</button>
-//           </div>
-//           <br><br>`;
-      
-//           document.querySelector('#syllabus-container').innerHTML += syllabusMarkup;
-//         })
+  let bodyData = {timelineTitle, timelineDesc, eventYear, eventTitle, eventDesc}
 
-//  }
+  fetch(URL, {
+    // POST request
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(bodyData)
+  })
+  .then(response => response.json())
+  .then(timeline => {
+    timeline.data.forEach(timeline => {
+        console.log(timeline.attributes.events);
+        const timelineMarkup = `
+        <div class="card bg-light">
+            <div class="card-body text-center">
+            <h4 class="card-text">${timeline.attributes.events.title}</h4>
+            <div class="card-body">
+            <p class="card-text">${timeline.attributes.events.description} </p>
+            <button type="button" class="btn btn-sm">Select</button>
+        </div>
+        <br><br>`;
 
+        document.querySelector('.card-columns').innerHTML += timelineMarkup
+    })
 })
+}
+
+ })
  
 
 // GET FETCH REQUEST - GET TIMELINE(S) TO DISPLAY
@@ -70,7 +69,7 @@ function getTimeline() {
     .then(timeline => {
         // console.log(timeline.data[1]);
         timeline.data.forEach(timeline => {
-            // console.log(timeline.attributes);
+            // console.log(timeline.attributes.events);
             const timelineMarkup = `
             <div class="card bg-light">
                 <div class="card-body text-center">
